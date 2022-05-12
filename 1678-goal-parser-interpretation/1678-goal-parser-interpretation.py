add68059d92947b014c11d7b396a05c9f65c037a
Vince Gonzales <vmgonzales@gmail.com>
2022-05-12 07:29:27 -0600

    Time: 22 ms (99.36%), Space: 13.7 MB (96.67%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,15 +1,21 @@
+#%% This was my first attempt: 11%/11%. Relatively poor performance.
+#class Solution:
+#    def interpret(self, command: str) -> str:
+#        output = ""
+#        while command:
+#            if command[0] == 'G':
+#                output += 'G'
+#                command = command[1: len(command)]
+#            elif command[0: 2] == '()':
+#                output += 'o'
+#                command = command[2: len(command)]
+#            elif command[0:4] == '(al)':
+#                output += 'al'
+#                command = command[2: len(command)]
+#            else: command = command[1: len(command)]
+#        return output
+    
+#%% Here's another approach with better performance.
 class Solution:
     def interpret(self, command: str) -> str:
-        output = ""
-        while command:
-            if command[0] == 'G':
-                output += 'G'
-                command = command[1: len(command)]
-            elif command[0: 2] == '()':
-                output += 'o'
-                command = command[2: len(command)]
-            elif command[0:4] == '(al)':
-                output += 'al'
-                command = command[2: len(command)]
-            else: command = command[1: len(command)]
-        return output
+        return command.replace('()','o').replace('(al)','al')
