49e1cf7effeaba5f586172a49709b71ac8f2c4dd
Vince Gonzales <vmgonzales@gmail.com>
2022-05-27 06:49:10 -0600

    Time: 144 ms (5.04%), Space: 14.5 MB (46.09%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,33 +1,23 @@
-# Solution with no pointers. Cleans string, then compares it to reversed string.
-# Runtime: 86.92%, but memory 21.67%
-
-# class Solution:
-#   def isPalindrome(self, s: str) -> bool:
-
-#     # Remove punctuation
-#     s = re.sub(r'[^a-zA-Z0-9]+', '', s)
-
-#     # Force lowercase
-#     s = s.lower()
-
-#     return s == s[::-1]
-
-import re
-
-# Here's another solution with two pointers
 class Solution:
-  def isPalindrome(self, s: str) -> bool:
-
-    # Remove punctuation
-    s = re.sub(r'[^a-zA-Z0-9]+', '', s)
-
-    # Force lowercase
-    s = s.lower()
-
-    first, last = 0, len(s)-1
-    while first < last:
-        if s[first] != s[last]: return False
-        first += 1
-        last -=1
+    def isPalindrome(self, s: str) -> bool:
+        
+        
+        first, last = 0, len(s)-1
+        while first < last:
+            # In Python, if you want to call a function inside an object,
+            # you have to use self.
+            while first < last and not self.alphaNum(s[first]):
+                first += 1
+            while first < last and not self.alphaNum(s[last]):
+                last -= 1
+                
+            if s[first].lower() != s[last].lower(): return False
+            first += 1
+            last -=1
+        
+        return True
     
-    return True
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9'))
