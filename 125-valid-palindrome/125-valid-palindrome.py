2497d6ac9476653593a60cedd2d5ba81c55b4450
Vince Gonzales <vmgonzales@gmail.com>
2022-05-27 05:52:11 -0600

    Time: 45 ms (90.93%), Space: 15.2 MB (26.95%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,3 +1,20 @@
+# Solution with no pointers. Cleans string, then compares it to reversed string.
+# Runtime: 86.92%, but memory 21.67%
+
+# class Solution:
+#   def isPalindrome(self, s: str) -> bool:
+
+#     # Remove punctuation
+#     s = re.sub(r'[^a-zA-Z0-9]+', '', s)
+
+#     # Force lowercase
+#     s = s.lower()
+
+#     return s == s[::-1]
+
+import re
+
+# Here's another solution with two pointers
 class Solution:
   def isPalindrome(self, s: str) -> bool:
 
@@ -7,4 +24,10 @@ class Solution:
     # Force lowercase
     s = s.lower()
 
-    return s == s[::-1]
+    first, last = 0, len(s)-1
+    while first < last:
+        if s[first] != s[last]: return False
+        first += 1
+        last -=1
+    
+    return True
